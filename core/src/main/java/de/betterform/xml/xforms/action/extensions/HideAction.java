81c098f4dc0a89d6dd93ac89a974762651c0b251
JoernT <joern.turner@googlemail.com>
2010-09-01 12:18:57 +0000

    actions for Dialog component (missed commit)

---
+++ core/src/main/java/de/betterform/xml/xforms/action/extensions/HideAction.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2010. betterForm Project - http://www.betterform.de
+ * Licensed under the terms of BSD License
+ */
+
+package de.betterform.xml.xforms.action.extensions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.betterform.xml.events.BetterFormEventNames;
+import de.betterform.xml.events.XFormsEventNames;
+import de.betterform.xml.xforms.model.Model;
+import de.betterform.xml.xforms.model.submission.Header;
+import de.betterform.xml.xforms.model.submission.Submission;
+import de.betterform.xml.xforms.action.AbstractAction;
+import de.betterform.xml.xforms.exception.XFormsBindingException;
+import de.betterform.xml.xforms.exception.XFormsException;
+import de.betterform.xml.xforms.ui.AbstractUIElement;
+import de.betterform.xml.xforms.XFormsElementFactory;
+import de.betterform.xml.xforms.XFormsConstants;
+import de.betterform.xml.xforms.XFormsElement;
+import de.betterform.xml.dom.DOMUtil;
+import de.betterform.xml.xpath.impl.saxon.XPathUtil;
+import de.betterform.xml.xpath.impl.saxon.XPathCache;
+import de.betterform.xml.ns.NamespaceConstants;
+import de.betterform.xml.ns.BetterFormNamespaceMap;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+
+/**
+ * Implements the hide action as defined in <a src="http://www.w3.org/MarkUp/Forms/wiki/Dialog">XForms 1.2 Dialog proposal</a>
+ *
+ * @author Ronald van Kuijk, based on work by
+ * @author Ulrich Nicolas Liss&eacute;
+ */
+public class HideAction extends AbstractAction {
+    private static Log LOGGER = LogFactory.getLog(HideAction.class);
+    private String referencedDialog = null;
+
+    /**
+     * Creates a setfocus action implementation.
+     *
+     * @param element the element.
+     * @param model the context model.
+     */
+    public HideAction(Element element, Model model) {
+        super(element, model);
+    }
+
+    // lifecycle methods
+
+    /**
+     * Performs element init.
+     */
+    public void init() throws XFormsException {
+        super.init();
+
+        if (this.referencedDialog == null) {
+            this.referencedDialog = getXFormsAttribute("dialog");
+        }
+        if(this.referencedDialog == null) {
+            throw new XFormsBindingException("missing dialog attribute at " + DOMUtil.getCanonicalPath(this.getElement()), this.target, null);
+        }
+    }
+
+    // implementation of 'de.betterform.xml.xforms.action.XFormsAction'
+
+    /**
+     * Performs the <code>setfocus</code> action.
+     *
+     * @throws XFormsException if an error occurred during <code>setfocus</code>
+     * processing.
+     */
+    public void perform() throws XFormsException {
+        // check dialog idref
+        Object dialogObject = this.container.lookup(this.referencedDialog);
+        if (dialogObject == null || !(dialogObject instanceof AbstractUIElement)) {
+            throw new XFormsBindingException("invalid control id at " + DOMUtil.getCanonicalPath(this.getElement()), this.target, this.referencedDialog);
+        }
+
+        // dispatch xforms-focus to form control
+        this.container.dispatch(((AbstractUIElement) dialogObject).getTarget(), BetterFormEventNames.HIDE, null);
+    }
+
+    /**
+     * Returns the logger object.
+     *
+     * @return the logger object.
+     */
+    protected Log getLogger() {
+        return LOGGER;
+    }
+}
+
+// end of class
