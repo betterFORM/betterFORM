2e4b991081b3b1ebe3671f0ee0082ad9b2a67a40
JoernT <joern.turner@googlemail.com>
2010-12-09 11:27:10 +0000

    - fix for #98

--- core/src/test/java/de/betterform/xml/xpath/XPathReferenceFinderTest.java
+++ core/src/test/java/de/betterform/xml/xpath/XPathReferenceFinderTest.java
@@ -77,11 +77,29 @@ public class XPathReferenceFinderTest extends TestCase {
                 this.referenceFinder.getReferences("count(instance(\"test\"))", Collections.EMPTY_MAP, fDummyContainer));
         assertReferences(new String[]{"instance(\"test\")/child::element(observation, xs:anyType)", "instance(\"test\")/child::element(observation, xs:anyType)/child::element(data, xs:anyType)", "instance(\"test\")"},
                 this.referenceFinder.getReferences("count(instance(\"test\")/observation[data != '']) > 0", Collections.EMPTY_MAP, fDummyContainer));
-        assertReferences(new String[]{"instance(\"test\")/child::element(observation, xs:anyType)", "instance(\"list\")/attribute::attribute(data, xs:anyAtomicType)", "instance(\"list\")", "instance(\"test\")/child::element(observation, xs:anyType)/child::element(data, xs:anyType)", "instance(\"test\")"},
+        /*
+            This test failed as reference finder returns xs:anySimpleType instead of xs:anyAtomicType as assumed by the assertion.
+
+            Now it seems that the assertion here was likely incorrect and/or the behavior of Saxon has changed in version 9. When you see definitions for the above types a xs:anyAtomicType is the superclass for all atomic values (meaning any value that is
+            atomic but not list types for instance). For the simple expression of an attribute as in this test below the test assumes to get back an xs:anyAtomicType but gets a xs:anySimpleType. But the latter seems
+            to be more correct as attributes surely can hold list types as values e.g. thinking of the HTML class attribute that can hold a list of atomic values.
+
+            As a consequence i adapt the test assertion to expect a xs:anySimpleType now and it runs green. Actually for the processing in betterFORM the type of the attribute value should be of no importance
+            at all so the test is just too restrictive here. Anyway i leave the original assertion here for reference and investigation for the unlikely case that problems should come up later on.
+         */
+//        assertReferences(new String[]{"instance(\"test\")/child::element(observation, xs:anyType)", "instance(\"list\")/attribute::attribute(data, xs:anyAtomicType)", "instance(\"list\")", "instance(\"test\")/child::element(observation, xs:anyType)/child::element(data, xs:anyType)", "instance(\"test\")"},
+//                this.referenceFinder.getReferences("count(instance(\"test\")/observation[data != instance('list')/@data]) > 0", Collections.EMPTY_MAP, fDummyContainer));
+
+        assertReferences(new String[]{"instance(\"test\")/child::element(observation, xs:anyType)", "instance(\"list\")/attribute::attribute(data, xs:anySimpleType)", "instance(\"list\")", "instance(\"test\")/child::element(observation, xs:anyType)/child::element(data, xs:anyType)", "instance(\"test\")"},
                 this.referenceFinder.getReferences("count(instance(\"test\")/observation[data != instance('list')/@data]) > 0", Collections.EMPTY_MAP, fDummyContainer));
+
+
         assertReferences(new String[]{"instance(\"editModes\")", "child::element(consumerId, xs:anyType)", "instance(\"version-data\")", "instance(\"editModes\")/child::element(editable, xs:anyType)", "instance(\"version-data\")/child::element(selectedVersion, xs:anyType)"},
                 this.referenceFinder.getReferences("concat(concat('sampleConsentQuestionnaireWorkflow?consumerId=', consumerId), IF(instance('version-data')/selectedVersion != '', concat('&versionNumber=', instance('version-data')/selectedVersion), ''), '&editable=', instance('editModes')/editable)", Collections.EMPTY_MAP, fDummyContainer));
-        assertReferences(new String[]{"instance(\"y\")/child::element(node, xs:anyType)", "instance(\"z\")", "child::element(element, xs:anyType)", "../child::element(no, xs:anyType)", "child::element(data, xs:anyType)", "..", "instance(\"x\")", "instance(\"y\")", "child::element(element, xs:anyType)/attribute::attribute(id, xs:anyAtomicType)", "(../child::element(no, xs:anyType))/child::element(data, xs:anyType)"},
+//        assertReferences(new String[]{"instance(\"y\")/child::element(node, xs:anyType)", "instance(\"z\")", "child::element(element, xs:anyType)", "../child::element(no, xs:anyType)", "child::element(data, xs:anyType)", "..", "instance(\"x\")", "instance(\"y\")", "child::element(element, xs:anyType)/attribute::attribute(id, xs:anyAtomicType)", "(../child::element(no, xs:anyType))/child::element(data, xs:anyType)"},
+//                this.referenceFinder.getReferences("concat(instance('x'), data or ../no/data, instance('y')/node, element[@id = instance('z')])", Collections.EMPTY_MAP, fDummyContainer));
+
+        assertReferences(new String[]{"instance(\"y\")/child::element(node, xs:anyType)", "instance(\"z\")", "child::element(element, xs:anyType)", "../child::element(no, xs:anyType)", "child::element(data, xs:anyType)", "..", "instance(\"x\")", "instance(\"y\")", "child::element(element, xs:anyType)/attribute::attribute(id, xs:anySimpleType)", "(../child::element(no, xs:anyType))/child::element(data, xs:anyType)"},
                 this.referenceFinder.getReferences("concat(instance('x'), data or ../no/data, instance('y')/node, element[@id = instance('z')])", Collections.EMPTY_MAP, fDummyContainer));
     }
 
