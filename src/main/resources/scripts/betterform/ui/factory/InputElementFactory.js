4def399560d7590700cb3d82192c0a76125bc65b
Tobi Krebs <tobias.krebs@betterform.de>
2011-03-18 14:47:14 +0100

    - Years-Range for DropDownDate can now be specified on the appearance attribute of the DropDownDate control

--- src/main/resources/scripts/betterform/ui/factory/InputElementFactory.js
+++ src/main/resources/scripts/betterform/ui/factory/InputElementFactory.js
@@ -16,7 +16,11 @@ dojo.declare(
 
         //TODO: ca deprecated?
         if (appearance != undefined && (appearance.indexOf("ca") != -1 || appearance.indexOf("bf:") != -1)) {
-            inputType = appearance;
+            if ( appearance.indexOf("=") != -1 ) {
+                inputType = appearance.substring(0, appearance.indexOf("="));
+            } else {
+                inputType = appearance;
+            }
             console.debug("Custom Input Type: appearance=" + appearance)
         }
 
@@ -91,27 +95,60 @@ dojo.declare(
                 if(xfValue == undefined){
                     xfValue = "";
                 }
-                var datePattern;
 
-                if (appearance.indexOf("iso8601:") != -1) {
-                    datePattern = appearance.substring(appearance.indexOf("iso8601:")+8);
-                    //console.debug("UIelementFactory.createWidget 1. datePattern:" + datePattern);
-                    if(datePattern.indexOf(" ") != -1) {
-                        datePattern = datePattern.substring(0,datePattern.indexOf(" ")).trim();
-                        //console.debug("UIelementFactory.createWidget 2. datePattern:" + datePattern);
+                var rangeStart;
+                var rangeEnd;
+
+                var now = new Date().getFullYear();
+                if (appearance.indexOf("=") != -1) {
+                    rangeStart = appearance.substring(appearance.indexOf("=")+1);
+
+
+                    if (rangeStart.indexOf(":") != -1) {
+                        rangeEnd = rangeStart.substring(rangeStart.indexOf(":")+1);
+                        rangeStart = rangeStart.substring(0, rangeStart.indexOf(":"));
                     }
+
+                    if (rangeStart.indexOf("-") != -1) {
+                        rangeStart = now - parseInt(rangeStart.substring(rangeStart.indexOf("-")+1));
+                    }
+
+                    if (rangeEnd != undefined) {
+                            if (rangeEnd.indexOf("+") != -1) {
+                                rangeEnd = now + parseInt(rangeEnd.substring(rangeEnd.indexOf("+")+1));
+                            }
+                    } else {
+                        rangeEnd = now;
+                    }
+
+                } else {
+                    rangeEnd = now;
+                    rangeStart = rangeEnd -10;
                 }
 
-                if (datePattern != undefined) {
+                rangeStart = parseInt(rangeStart);
+                rangeEnd = parseInt(rangeEnd);
+
+                var templateString = "<div class='xfDropDownDateControl'><input class='xfValue' type='hidden' dojoAttachPoint='valueNode' value=''/><span class='xfDropDownDate'><select size='1' dojoType='dijit.form.ComboBox' dojoAttachPoint='daysFacet' class='xfDropDownDateDays'><option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option><option>07</option><option>08</option><option>09</option><option>10</option><option>11</option><option>12</option><option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option><option>21</option><option>22</option><option>23</option><option>24</option><option>25</option><option>26</option><option>27</option><option>28</option><option>29</option><option>30</option><option>31</option></select><select size='1' dojoType='dijit.form.ComboBox' dojoAttachPoint='monthsFacet' class='xfDropDownDateMonths'><option value='01'>January</option><option value='02'>February</option><option value='03'>March</option><option value='04'>April</option><option value='05'>May</option><option value='06'>June</option><option value='07'>July</option><option value='08'>August</option><option value='09'>September</option><option value='10'>October</option><option value='11'>November</option><option value='12'>December</option></select><select size='1' dojoType='dijit.form.ComboBox' dojoAttachPoint='yearsFacet' class='xfDropDownDateYears'>";
+                var end = "</select></span></div>";
+
+                for (var i = rangeStart; i <= rangeEnd; i++) {
+                    templateString = templateString + "<option>" + i + "</option>";
+                }
+                templateString = templateString + end;
+
+
                     try {
                         newInputDateWidget = new betterform.ui.input.DropDownDate({
                             name:controlId + "-value",
                             value:xfValue,
                             "class":classValue,
                             title:dojo.attr(sourceNode, "title"),
+                            rangeStart:rangeStart,
+                            rangeEnd:rangeEnd,
+                            templateString: templateString,
                             constraints:{
-                                selector:'date',
-                                datePattern:datePattern
+                                selector:'date'
                             },
                             xfControlId:controlId
                         },
@@ -120,18 +157,7 @@ dojo.declare(
                     catch (ex) {
                         alert(ex)
                     }
-                } else {
-                    newInputDateWidget = new betterform.ui.input.DropDownDate({
-                        name:controlId + "-value",
-                        value:xfValue,
-                        "class":classValue,
-                        title:dojo.attr(sourceNode,"title"),
-                        constraints:{
-                            selector:'date'
-                        },
-                        xfControlId:controlId
-                    }, sourceNode);
-                }
+
                 return newInputDateWidget;
     },
 
--- src/main/resources/scripts/betterform/ui/input/DropDownDate.js
+++ src/main/resources/scripts/betterform/ui/input/DropDownDate.js
@@ -11,13 +11,12 @@ dojo.declare(
 {
     templateString: dojo.cache("betterform", "ui/templates/DropDownDate.html"),
     widgetsInTemplate:true,
+    rangeStart: 1900,
+    rangeEnd:0,
     years:0,
     months:0,
     days:0,
-
-    constructor:function() {
-        console.debug("DropDownDate.constructor");
-    },
+    monthsArray: new Array( "January","February","March","April","May","June","July","August","September","October","November","December"),
 
     postMixInProperties:function() {
         console.debug("DropDownDate.postMixInProperties");
@@ -28,18 +27,19 @@ dojo.declare(
     postCreate:function() {
         console.debug("DropDownDate.postCreate this.domNode:", this.valueNode, " this.value: ", this.value);
         this.inherited(arguments);
+
         dojo.attr(this.valueNode, "value", this.value);
         this.applyValues();
-
         dojo.connect(dijit.byId(this.daysFacet.id), "onChange", this, "onDaysChanged");
         dojo.connect(dijit.byId(this.monthsFacet.id), "onChange", this, "onMonthsChanged");
         dojo.connect(dijit.byId(this.yearsFacet.id), "onChange", this, "onYearsChanged");
-        dojo.connect(dijit.byId(this.yearsFacet.id), "onBlur", this, "onYearsBlur");
+        //dojo.connect(dijit.byId(this.yearsFacet.id), "onBlur", this, "onYearsBlur");
     },
 
     applyValues:function() {
         console.debug("DropDownDate.applyValues value:",this.value);
         if (this.value != undefined) {
+
             var dropDownDateContainer = this.value.split("-");
             if(dropDownDateContainer.length != 3) {
                 return;
@@ -50,27 +50,48 @@ dojo.declare(
             months = dropDownDateContainer[1];
             days = dropDownDateContainer[2];
 
-            dijit.byId(this.daysFacet.id).setValue(days);
-            dijit.byId(this.monthsFacet.id).setValue(months);
-            dijit.byId(this.yearsFacet.id).setValue(years);
+            dijit.byId(this.daysFacet.id).set('value', days);
+            dijit.byId(this.monthsFacet.id).set('value',this.monthsArray[parseInt(months)-1]);
+            //dijit.byId(this.monthsFacet.id).set('displayValue', this.monthsArray[parseInt(months)-1]);
+            dijit.byId(this.yearsFacet.id).set('value', years);
          }
     },
 
     onDaysChanged:function(evt) {
         console.debug("DropDownDate.onDaysChanged.");
-        this.days = dijit.byId(this.daysFacet.id).getValue();
+        var selectedItem = dijit.byId(this.daysFacet.id).get('item');
+        if (selectedItem != undefined) {
+            this.days =  selectedItem.value;
+        } else {
+            this.days =  dijit.byId(this.daysFacet.id).getValue();
+        }
         this.setCurrentDate();
     },
 
     onMonthsChanged:function(evt) {
         console.debug("DropDownDate.onMonthsChanged.");
-        this.months = dijit.byId(this.monthsFacet.id).getValue();
+        var selectedItem = dijit.byId(this.monthsFacet.id).get('item');
+        if (selectedItem != undefined) {
+            this.months =  selectedItem.value;
+        } else {
+            var month = dijit.byId(this.monthsFacet.id).getValue();
+            if (isNaN(month)) {
+                this.months =  this.monthsArray.indexOf(month) +1;
+            } else {
+                this.months = month;
+            }
+        }
         this.setCurrentDate();
     },
 
     onYearsChanged:function(evt) {
         console.debug("DropDownDate.onYearsChanged.");
-        this.years = dijit.byId(this.yearsFacet.id).getValue();
+        var selectedItem = dijit.byId(this.yearsFacet.id).get('item');
+        if (selectedItem != undefined) {
+            this.years =  selectedItem.value;
+        } else {
+            this.years =  dijit.byId(this.yearsFacet.id).getValue();
+        }
         this.setCurrentDate();
     },
 
@@ -101,9 +122,11 @@ dojo.declare(
     },
 
      setCurrentDate:function(){
+         console.debug("betterform.ui.input.DropDownDate.setCurrentDate computeDate: ", this.years + "-" + this.months + "-" + this.days);
          var currentDate = this.years + "-" + this.months + "-" + this.days;
          console.debug("betterform.ui.input.DropDownDate.setCurrentDate currentDate: ", currentDate);
          dojo.attr(this.valueNode, "value", currentDate);
          this.value = currentDate;
+         this.setControlValue();
      }
 });
