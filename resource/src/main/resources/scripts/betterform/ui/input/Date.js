551dfe7906cf48699c64c8af00a4f7399d7c489d
Lars Windauer <lars.windauer@betterform.de>
2011-08-26 11:42:57 +0200

    [bugfix] fixed validation of date control

--- resource/src/main/resources/scripts/betterform/ui/input/Date.js
+++ resource/src/main/resources/scripts/betterform/ui/input/Date.js
@@ -14,14 +14,12 @@ dojo.declare(
         "betterform.ui.input.Date",
         [betterform.ui.ControlValue, dijit.form.DateTextBox],
 {
-    constructor:function() {
-       this.incremental = true;
-    },
 
     postMixInProperties:function() {
         this.inherited(arguments);
         this.applyProperties(dijit.byId(this.xfControlId), this.srcNodeRef);
-        /* incremental date does not make sense, if wanted anyway comment in the following line of code and implement _valueChange */
+        this.incremental = false;
+               /* incremental date does not make sense, if wanted anyway comment in the following line of code and implement _valueChange */
         // dojo.connect(this,"_onKeyPress", this,"_valueChanged");
     },
     postCreate:function() {
