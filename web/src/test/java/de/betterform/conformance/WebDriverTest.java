40de7e461f3aeb25a695ce1b472e673866202556
robertnetz <robert@netzschwitz.de>
2012-01-27 16:18:48 +0100

    [Webtest] timeout set to 10 secs. to decrease test execution time

--- web/src/test/java/de/betterform/conformance/WebDriverTest.java
+++ web/src/test/java/de/betterform/conformance/WebDriverTest.java
@@ -45,7 +45,7 @@ public abstract class WebDriverTest implements WebDriverTestInterface {
             }
         }
 
-        this.webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        this.webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
