99ec47952650a4803f22b93eb07365518ce3d0cf
JoernT <joern.turner@betterform.de>
2012-04-17 11:40:57 +0200

    -updated mindmap

--- resource/src/main/doc/xhtml5.mm
+++ resource/src/main/doc/xhtml5.mm
@@ -2,6 +2,28 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1334312641624" ID="ID_532793562" MODIFIED="1334587335917" TEXT="xhtml5">
 <hook NAME="accessories/plugins/RevisionPlugin.properties"/>
+<node CREATED="1334654899187" HGAP="50" ID="ID_687230137" MODIFIED="1334655029162" POSITION="left" TEXT="Symbole" VSHIFT="-74">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1334654908889" ID="ID_181491995" MODIFIED="1334654983093" TEXT="bitte besondere beachten - priorit&#xe4;r bearbeiten">
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1334654925576" ID="ID_1746094778" MODIFIED="1334654934241" TEXT="done">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334654999780" ID="ID_1311051311" MODIFIED="1334655008982" TEXT="open question">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1334655013690" ID="ID_515562240" MODIFIED="1334655023489" TEXT="Idee (f&#xfc;r sp&#xe4;ter)">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1334655045641" ID="ID_877268362" MODIFIED="1334655057177" TEXT="Priorit&#xe4;ten">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
 <node CREATED="1334312729819" ID="ID_1850248356" MODIFIED="1334568175291" POSITION="left" TEXT="missing controls">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -15,26 +37,62 @@
 </html></richcontent>
 <font NAME="SansSerif" SIZE="18"/>
 <node CREATED="1334312734193" ID="ID_1891416982" MODIFIED="1334312736849" TEXT="Groups"/>
-<node CREATED="1334312737851" ID="ID_625967465" MODIFIED="1334312744094" TEXT="Range (Star)"/>
-<node CREATED="1334312746737" ID="ID_1267714804" MODIFIED="1334312755481" TEXT="Uploads">
-<node CREATED="1334312762791" ID="ID_823972280" MODIFIED="1334312764288" TEXT="anyURI"/>
-<node CREATED="1334312765290" ID="ID_1886192199" MODIFIED="1334312768619" TEXT="base64"/>
-<node CREATED="1334589092797" ID="ID_1728552978" MODIFIED="1334589098212" TEXT="hexBinary"/>
+<node CREATED="1334312737851" ID="ID_625967465" MODIFIED="1334655292052" TEXT="Range (Star)">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1334312746737" ID="ID_1267714804" MODIFIED="1334654749179" TEXT="Uploads">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1334312762791" ID="ID_823972280" MODIFIED="1334655276581" TEXT="anyURI">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1334312765290" ID="ID_1886192199" MODIFIED="1334655282028" TEXT="base64">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1334589092797" ID="ID_1728552978" MODIFIED="1334655285175" TEXT="hexBinary">
+<icon BUILTIN="full-4"/>
+</node>
 </node>
 <node CREATED="1334312782366" ID="ID_782540235" MODIFIED="1334312785101" TEXT="Textarea">
-<node CREATED="1334312791581" ID="ID_1290528025" MODIFIED="1334312794315" TEXT="HTML Editor"/>
+<node CREATED="1334312791581" ID="ID_1290528025" MODIFIED="1334655265396" TEXT="HTML Editor">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      der HTML Editor ist ein gutes Beispiel f&#252;r eine optionale Komponente und sollte ggf. in einem eigenen Layer untergebracht werden.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+</node>
 </node>
 <node CREATED="1334312798004" ID="ID_1306089" MODIFIED="1334587024623" TEXT="Input">
 <font NAME="SansSerif" SIZE="16"/>
-<node CREATED="1334312802428" ID="ID_910667397" MODIFIED="1334312804366" TEXT="DateTime"/>
-<node CREATED="1334312808230" ID="ID_692743157" MODIFIED="1334312808949" TEXT="Time"/>
+<node CREATED="1334312802428" ID="ID_910667397" MODIFIED="1334655254028" TEXT="DateTime">
+<icon BUILTIN="full-1"/>
 </node>
-<node CREATED="1334312812169" ID="ID_1301387691" MODIFIED="1334312822507" TEXT="Select(1)">
+<node CREATED="1334312808230" ID="ID_692743157" MODIFIED="1334655257928" TEXT="Time">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1334312812169" ID="ID_1301387691" MODIFIED="1334655222292" TEXT="Select(1)">
+<icon BUILTIN="full-2"/>
 <node CREATED="1334312823037" ID="ID_459754586" MODIFIED="1334312829540" TEXT="Open Select(1)"/>
 </node>
-<node CREATED="1334312830618" ID="ID_319478051" MODIFIED="1334312832588" TEXT="Switch"/>
+<node CREATED="1334312830618" ID="ID_319478051" MODIFIED="1334312832588" TEXT="Switch">
+<node CREATED="1334591579152" ID="ID_39599196" MODIFIED="1334591587399" TEXT="plain">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334591614239" ID="ID_1320494923" MODIFIED="1334655238488" TEXT="TabContainer">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
 <node CREATED="1334312844816" ID="ID_293627423" MODIFIED="1334589065991" TEXT="Trigger">
-<node CREATED="1334312848145" ID="ID_1862534914" MODIFIED="1334312850879" TEXT="Minimal"/>
+<node CREATED="1334312848145" ID="ID_1862534914" MODIFIED="1334590220097" TEXT="Minimal">
+<icon BUILTIN="button_ok"/>
+</node>
 <node CREATED="1334312859442" ID="ID_717792717" MODIFIED="1334589069578" TEXT="Image">
 <icon BUILTIN="button_ok"/>
 </node>
@@ -81,21 +139,38 @@
 </node>
 </node>
 <node CREATED="1334312937269" ID="ID_658451491" MODIFIED="1334319295924" POSITION="left" TEXT="known bugs / issues">
-<node CREATED="1334312684148" ID="ID_552097956" MODIFIED="1334568187977" TEXT="behaviors &amp; xsdTypes">
+<node CREATED="1334312684148" ID="ID_552097956" MODIFIED="1334655158523" TEXT="behaviors &amp; xsdTypes">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ist noch zu kl&#228;ren, wie der baseType konkret in den Matchingrules verwendet wird. Zun&#228;chst ist sicher der baseType als default zu verwenden. Muss noch gekl&#228;rt werden, wie eine abweichende Regel definiert wird.
+    </p>
+  </body>
+</html>
+</richcontent>
 <font NAME="SansSerif" SIZE="15"/>
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="help"/>
 <node CREATED="1334312706034" ID="ID_1685225063" MODIFIED="1334312722403" TEXT="we need to have the xsd basetype for unknown xsd types"/>
 </node>
 <node CREATED="1334313161965" ID="ID_1775336959" MODIFIED="1334313168472" TEXT="Repeat">
-<node CREATED="1334313164226" ID="ID_1049424695" MODIFIED="1334313165260" TEXT="Repeat: the RepeatIndex does not get changed correctly if the index(&apos;repeatId&apos;) function is used for insert / delete actions"/>
-<node CREATED="1334313228123" ID="ID_1787087565" MODIFIED="1334332255510" TEXT="Switching a RepeatFull to readonly does not work correctly, handleStateChanged events are only present for repeat items but not the repeat itself."/>
+<node CREATED="1334313164226" ID="ID_1049424695" MODIFIED="1334654880234" TEXT="Repeat: the RepeatIndex does not get changed correctly if the index(&apos;repeatId&apos;) function is used for insert / delete actions">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1334313177759" ID="ID_1196653002" MODIFIED="1334313194545" TEXT="DropDownDate">
-<node CREATED="1334313195418" ID="ID_331816711" MODIFIED="1334313195418" TEXT="validation of DropDownDate (if bf:dropdowndata=xyz is used there must be a corresponding constraint on the bind)"/>
+<node CREATED="1334313228123" ID="ID_1787087565" MODIFIED="1334654871468" TEXT="Switching a RepeatFull to readonly does not work correctly, handleStateChanged events are only present for repeat items but not the repeat itself.">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
 </node>
 <node CREATED="1334313201936" ID="ID_143705202" MODIFIED="1334313204266" TEXT="Select(1)">
-<node CREATED="1334313204801" ID="ID_196888749" MODIFIED="1334313204801" TEXT="xforms-select events within itemsets do not work"/>
+<node CREATED="1334313204801" ID="ID_196888749" MODIFIED="1334655507769" TEXT="xforms-select events within itemsets do not work">
+<icon BUILTIN="full-1"/>
 </node>
-<node CREATED="1334313240637" ID="ID_1908766117" MODIFIED="1334313422459" TEXT="Outputs as Labels">
+</node>
+<node CREATED="1334313240637" ID="ID_1908766117" MODIFIED="1334655516385" TEXT="Outputs as Labels">
+<icon BUILTIN="full-2"/>
 <node CREATED="1334313244807" ID="ID_1971874188" MODIFIED="1334587327150" TEXT="Using Outputs within Labels (e.g. of a trigger) does not work failsave yet">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -122,23 +197,70 @@
 </node>
 </node>
 <node CREATED="1334319297739" ID="ID_903626495" MODIFIED="1334319300018" TEXT="Group">
-<node CREATED="1334319300019" ID="ID_1046566464" MODIFIED="1334319334870" TEXT="Group: bf:horizontalTable does not render correctly"/>
+<node CREATED="1334319300019" ID="ID_1046566464" MODIFIED="1334655528353" TEXT="Group: bf:horizontalTable does not render correctly">
+<icon BUILTIN="full-3"/>
+</node>
 <node CREATED="1334319336715" ID="ID_1431910970" MODIFIED="1334319342036" TEXT="Test Groups within Repeats"/>
 <node CREATED="1334319415445" ID="ID_244483394" MODIFIED="1334319440522" TEXT="Relevance does not work for bf:horizontalTable and bf:verticalTable"/>
 </node>
-<node CREATED="1334587687721" ID="ID_715812072" MODIFIED="1334587711471" TEXT="repair debug bar - instance links not working"/>
-<node CREATED="1334587726182" ID="ID_1658748963" MODIFIED="1334587731794" TEXT="repair devtool"/>
+<node CREATED="1334587687721" ID="ID_715812072" MODIFIED="1334655571981" TEXT="repair debug bar - instance links not working">
+<icon BUILTIN="full-2"/>
 </node>
-<node CREATED="1334332332449" ID="ID_476499108" MODIFIED="1334568166195" POSITION="right" TEXT="build (Dojo Release)">
-<font NAME="SansSerif" SIZE="21"/>
+<node CREATED="1334587726182" ID="ID_1658748963" MODIFIED="1334655576539" TEXT="repair devtool">
+<icon BUILTIN="full-3"/>
 </node>
-<node CREATED="1334332341302" ID="ID_1942386648" MODIFIED="1334332351368" POSITION="right" TEXT="Mobile"/>
-<node CREATED="1334332352766" ID="ID_1492704790" MODIFIED="1334568163570" POSITION="right" TEXT="Performance">
+<node CREATED="1334313177759" ID="ID_1196653002" MODIFIED="1334655490163" TEXT="DropDownDate">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      validation of DropDownDate (if bf:dropdowndata=xyz is used there must be a corresponding constraint on the bind)
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1334332332449" ID="ID_476499108" MODIFIED="1334655171213" POSITION="right" TEXT="build (Dojo Release)">
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1334332341302" ID="ID_1942386648" MODIFIED="1334655344720" POSITION="right" TEXT="Mobile" VSHIFT="11">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      was ist hier zu tun? Showcase f&#252;r Date z.B. (Featuredetection?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1334332352766" ID="ID_1492704790" MODIFIED="1334655314860" POSITION="right" TEXT="Performance" VSHIFT="24">
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 <node CREATED="1334584796751" HGAP="21" ID="ID_1143421039" MODIFIED="1334584825541" POSITION="right" TEXT="test" VSHIFT="81">
 <font NAME="SansSerif" SIZE="23"/>
-<node CREATED="1334584808228" ID="ID_1606766015" MODIFIED="1334584811699" TEXT="load embed"/>
+<node CREATED="1334584808228" ID="ID_1606766015" MODIFIED="1334655184895" TEXT="load embed">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1334591363142" ID="ID_202310583" MODIFIED="1334591378650" TEXT="incremental behavior">
+<node CREATED="1334591378650" ID="ID_1163386783" MODIFIED="1334591381884" TEXT="input"/>
+<node CREATED="1334591382348" ID="ID_1938889919" MODIFIED="1334591385944" TEXT="select"/>
+<node CREATED="1334591386429" ID="ID_234545542" MODIFIED="1334591391427" TEXT="select1"/>
+<node CREATED="1334591391844" ID="ID_179128310" MODIFIED="1334591395978" TEXT="range"/>
+<node CREATED="1334591396835" ID="ID_1624608768" MODIFIED="1334591399178" TEXT="secret"/>
+<node CREATED="1334591399779" ID="ID_179995382" MODIFIED="1334591403106" TEXT="textarea"/>
+<node CREATED="1334591404092" ID="ID_543044728" MODIFIED="1334591413142" TEXT="upload"/>
+</node>
+<node CREATED="1334655538643" ID="ID_285195655" MODIFIED="1334655554390" TEXT="nested repeats">
+<icon BUILTIN="full-2"/>
+</node>
 </node>
 </node>
 </map>
