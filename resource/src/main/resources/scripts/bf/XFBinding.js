e77343e6c142b272c50c64e1c0a14b102634ea76
Lars Windauer <lars.windauer@betterform.de>
2013-01-16 12:22:16 +0100

    - JS "hack" to handle labels manipulated by dojo (dojo rewrites label ids of dijit/form/ComboBox, e.q. 'C4-label' becomes 'C4-value_label')

--- resource/src/main/resources/scripts/bf/XFBinding.js
+++ resource/src/main/resources/scripts/bf/XFBinding.js
@@ -221,7 +221,7 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
             },
 
             setEnabled:function() {
-                var label = dom.byId(this.id + "-label");
+                var label = this._getLabel();
                 if (label != undefined) {
                     if (domClass.contains(label, "xfDisabled")) {
                         domClass.replace(label, "xfEnabled", "xfDisabled");
@@ -239,7 +239,7 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
             },
 
             setDisabled:function() {
-                var label = dom.byId(this.id + "-label");
+                var label = this._getLabel();
                 if (label != undefined) {
                     if (domClass.contains(label, "xfEnabled")) {
                         domClass.replace(label,"xfDisabled", "xfEnabled");
@@ -256,7 +256,7 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
             },
 
             _handleHelperChanged: function(properties) {
-                // console.debug("Control.handleHelperChanged: this.id: "+this.id+ "type='" + properties["targetName"] + "',  value='" + properties["value"] + "'");
+                // console.debug("XFBinding.handleHelperChanged: this.id: "+this.id+ " type='" + properties["targetName"] + "',  value='" + properties["value"] + "'");
                 switch (properties["targetName"]) {
                     case "label":
                         this.setLabel(properties["value"]);
@@ -270,13 +270,15 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
                     case "alert":
                         this.setAlert(properties["value"]);
                         return;
+                    default:
+                        console.warn("XFBinding._handleHelperChange: can't handle ", properties["targetName"]);
                 }
             },
 
             setLabel:function(value) {
                 // console.debug("XFBinding.setLabel value:"+ value);
 
-                var labelNode = dom.byId(this.id + "-label");
+                var labelNode = this._getLabel();
                 if (labelNode != undefined) {
                     labelNode.innerHTML = value;
                 }
@@ -353,6 +355,16 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
 
             },
 
+            // function to get the label for a specific control, handles label ids manipulated by Dojo as well
+            _getLabel: function() {
+                var label = dom.byId(this.id + "-label");
+                if(label == undefined) {
+                    label = dom.byId(this.id + "-value_label");
+                }
+                return label;
+            },
+
+
             getWidget:function() {
                 if(domClass.contains(this.srcNodeRef,"xfContainer")){
                     if(this.widget == undefined) {
@@ -370,5 +382,5 @@ define(["dojo/_base/declare","dojo/dom", "dojo/dom-class","dojo/query",
         });
     }
 );
-    
+
 
