6fb90df396da01b7259d19872fd571488cea9933
Lars Windauer <lars.windauer@betterform.de>
2013-04-11 16:41:52 +0200

    * cornerstone for betterFORM URIResolvers to support execution of XQuery Module Functions (WARNING: ExistURIResolver is not generalized yet, currently only(!) the function testdata:test is called statically)

--- core/src/main/java/de/betterform/connector/exist/ExistURIResolver.java
+++ core/src/main/java/de/betterform/connector/exist/ExistURIResolver.java
@@ -13,6 +13,7 @@ import org.apache.commons.logging.LogFactory;
 import org.exist.EXistException;
 import org.exist.dom.BinaryDocument;
 import org.exist.dom.DocumentImpl;
+import org.exist.dom.QName;
 import org.exist.security.PermissionDeniedException;
 import org.exist.security.xacml.AccessContext;
 import org.exist.source.DBSource;
@@ -23,14 +24,8 @@ import org.exist.storage.lock.Lock;
 import org.exist.storage.serializers.Serializer;
 import org.exist.util.MimeType;
 import org.exist.xmldb.XmldbURI;
-import org.exist.xquery.CompiledXQuery;
-import org.exist.xquery.XPathException;
-import org.exist.xquery.XQuery;
-import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.Item;
-import org.exist.xquery.value.NodeValue;
-import org.exist.xquery.value.Sequence;
-import org.exist.xquery.value.Type;
+import org.exist.xquery.*;
+import org.exist.xquery.value.*;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -70,12 +65,17 @@ public class ExistURIResolver extends AbstractHTTPConnector implements URIResolv
 
 
         Map appContext=this.getContext();
-        Object test = appContext.get("key");
         BrokerPool pool = null;
         DBBroker broker = null;
         DocumentImpl xmlResource = null;
         try {
             URI dbURI = new URI(uriString);
+            String rawFragment= dbURI.getRawFragment();
+            String fragment= dbURI.getFragment();
+            String query = dbURI.getQuery();
+            String rawquery = dbURI.getQuery();
+
+
             pool = BrokerPool.getInstance();
             broker = pool.get(pool.getSecurityManager().getGuestSubject());
             xmlResource = broker.getResource(XmldbURI.createInternal(dbURI.getRawPath()), Lock.READ_LOCK);
@@ -87,25 +87,49 @@ public class ExistURIResolver extends AbstractHTTPConnector implements URIResolv
             }
             else if(xmlResource.getResourceType() == DocumentImpl.XML_FILE){
                 return serializer.serialize(xmlResource);
-            } else {
-                xmlResource.getMetadata().getMimeType();
-                if(MimeType.XQUERY_TYPE.getName().equals(xmlResource.getMetadata().getMimeType())){
+            } else if(MimeType.XQUERY_TYPE.getName().equals(xmlResource.getMetadata().getMimeType())){
+                Sequence resultSequence = null;
+
+                boolean isModule = true;
+                // check if the referenced resource is an XQuery
+                // TODO: parse URI to check if we have to handle an XQuery or XQuery module
+                if(isModule){
+
+                    XQueryContext tempContext = new XQueryContext(pool, AccessContext.REST);
+                    tempContext.setModuleLoadPath(xmlResource.getCollection().getURI().toString());
+                    Module module = tempContext.importModule(null, null, "xmldb:exist://"+dbURI.getRawPath());
+
+                    // UserDefinedFunction udf = tempContext.resolveFunction(new QName("test", module.getNamespaceURI(),module.getDefaultPrefix()),0);
+                    // TODO: replace 0 with calculated quantity
+                    UserDefinedFunction udf = ((ExternalModule)module).getFunction(new QName("test", module.getNamespaceURI(), module.getDefaultPrefix()), 0, tempContext);
+
+                    final FunctionReference fnRef = new FunctionReference(new FunctionCall(tempContext, udf));
+                    fnRef.analyze(new AnalyzeContextInfo());
+                    // fill new Sequence[]{} with function parameters in correct order
+                    // can be only
+                    // new StringValue("hallo")
+                    resultSequence = fnRef.evalFunction(null, null, new Sequence[]{});
+                    // TODO: reset should be called on fnRef
+                    udf.resetState(true);
+
+                }else {
                     Source source = new DBSource(broker, ((BinaryDocument)xmlResource),true);
                     XQuery xquery = broker.getXQueryService();
                     XQueryContext context = xquery.newContext(AccessContext.REST);
                     context.setModuleLoadPath(xmlResource.getCollection().getURI().toString());
                     context.setStaticallyKnownDocuments(new XmldbURI[]{xmlResource.getCollection().getURI()});
                     CompiledXQuery compiledXQuery = xquery.compile(context, source);
-                    Sequence sequence = xquery.execute(compiledXQuery, Sequence.EMPTY_SEQUENCE);
-                    if(sequence.getItemCount() != 1){
-                        throw new XFormsException("XML is not well formed");
+                    resultSequence = xquery.execute(compiledXQuery, Sequence.EMPTY_SEQUENCE);
+                }
+                // verify results
+                if(resultSequence == null || resultSequence.getItemCount() != 1){
+                    throw new XFormsException("XML is not well formed");
+                } else {
+                    Item item = resultSequence.itemAt(0);
+                    if(Type.subTypeOf(item.getType(), Type.NODE)) {
+                        return serializer.serialize((NodeValue)item);
                     }else {
-                         Item item = sequence.itemAt(0);
-                        if(Type.subTypeOf(item.getType(), Type.NODE)) {
-                            return serializer.serialize((NodeValue)item);
-                        }else {
-                            throw new XFormsException("The xquery did not return a valid XML node");
-                        }
+                        throw new XFormsException("The xquery did not return a valid XML node");
                     }
                 }
             }
