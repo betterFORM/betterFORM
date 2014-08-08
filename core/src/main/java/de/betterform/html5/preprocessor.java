99a63059dba37716237861ae90266ff8f0d47c75
JoernT <joern.turner@betterform.de>
2014-08-08 11:48:03 +0200

    unit test and processor stub

--- core/src/main/java/de/betterform/html5/preprocessor.java
+++ core/src/main/java/de/betterform/html5/preprocessor.java
@@ -7,13 +7,15 @@ import de.betterform.xml.xslt.impl.CachingTransformerService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.InputSource;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 
 /**
  * takes HTML5 input, parses it and turns it into an XML representation that will be transformed to
@@ -24,10 +26,22 @@ public class Preprocessor {
     public Preprocessor(){
     }
 
+    public static Node submit2Xforms(String url, CachingTransformerService cachingTransformerService, String data)
+            throws URISyntaxException, TransformerException, XFormsConfigException, IOException {
+        URL uri = new URL(url);
+        Document doc = Jsoup.parse(uri, 1000);
+        org.w3c.dom.Document domDoc = DOMBuilder.jsoup2DOM(doc);
+        String styles = Config.getInstance().getProperty("preprocessor-transform");
+        Transformer transformer = cachingTransformerService.getTransformerByName(styles);
+        transformer.setParameter("data",data);
+        DOMResult domResult = new DOMResult();
+        transformer.transform(new DOMSource(domDoc), domResult);
+        return domResult.getNode();
+    }
+
     public static Node html2Xforms(String html5, CachingTransformerService cachingTransformerService) {
         Document doc = Jsoup.parse(html5);
         org.w3c.dom.Document domDoc = DOMBuilder.jsoup2DOM(doc);
-
         try {
             String styles = Config.getInstance().getProperty("preprocessor-transform");
             Transformer transformer = cachingTransformerService.getTransformerByName(styles);
@@ -39,7 +53,7 @@ public class Preprocessor {
         } catch (XFormsConfigException e) {
             e.printStackTrace();
         }
-
         return null;
     }
+
 }
---
+++ core/src/main/java/de/betterform/xml/xforms/ModelProcessor.java
@@ -0,0 +1,26 @@
+package de.betterform.xml.xforms;
+
+import de.betterform.xml.xforms.exception.XFormsException;
+import org.w3c.dom.events.Event;
+
+/**
+ * processes standalong XForms models.
+ */
+public class ModelProcessor extends AbstractProcessorDecorator {
+
+    public ModelProcessor() {
+        super();
+    }
+
+    @Override
+    public void init() throws XFormsException {
+
+    }
+
+    @Override
+    public void handleEvent(Event event) {
+
+    }
+
+
+}
